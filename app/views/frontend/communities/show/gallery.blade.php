771d9148d0773ce70c2ffa039c3237950b15924a
Brandon Pierce <brandonpierce91@gmail.com>
2014-05-21 10:14:16 -0400

    small additions

--- app/views/frontend/communities/show/gallery.blade.php
+++ app/views/frontend/communities/show/gallery.blade.php
@@ -1,3 +1,16 @@
 <div class="gallery">
-	gallery
+  <div class="main-image-container thumbnail">
+    <div class="main-image-inner">
+      <img src="{{ $community->main_image }}" class="img-responsive img-rounded" />
+    </div>
+  </div>
+  <div class="row">
+  	@foreach ($community->images as $image)
+      <div class="col-md-2">
+        <div class="gallery-img-container">
+          <img src="{{ $image->path }}" class="img-responsive img-thumbnail equal-height" alt="{{ $image->name }}" />
+        </div>
+      </div>
+    @endforeach
+  </div>
 </div>
