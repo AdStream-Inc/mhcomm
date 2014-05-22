aa66f12335eafde815359dfcbe6e30a0a37da408
davidwebber10 <david@adstreaminc.com>
2014-05-22 10:47:44 -0400

    revision migrations

---
+++ app/database/migrations/2014_05_21_230058_add_fields_to_revision_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddFieldsToRevisionTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('revisions', function(Blueprint $table) {
+			
+			$table->string('parent_type');
+			$table->integer('parent_id');
+			
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('revisions', function(Blueprint $table) {
+			
+			$table->dropColumn('parent_type');
+			$table->dropColumn('parent_id');
+			
+		});
+	}
+
+}
