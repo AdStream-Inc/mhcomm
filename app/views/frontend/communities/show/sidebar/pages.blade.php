d6503c2bc7a03384668ce819eee4c114e48eb0ba
Brandon Pierce <brandonpierce91@gmail.com>
2014-08-11 15:48:34 -0400

    pages fix

--- app/views/frontend/communities/show/sidebar/pages.blade.php
+++ app/views/frontend/communities/show/sidebar/pages.blade.php
@@ -1,15 +1,17 @@
 @foreach ($pages as $page)
-  <li>
-    <a @if ($activePage == $page->id) class="active" @endif href="{{ url('communities/' . $slug . '/' . $page->slug . '.html') }}">
-      @if ($page->icon)
-        <span class="fa {{ $page->icon }} sidebar-icon"></span>
+  @if ($page->enabled)
+    <li>
+      <a @if ($activePage == $page->id) class="active" @endif href="{{ url('communities/' . $slug . '/' . $page->slug . '.html') }}">
+        @if ($page->icon)
+          <span class="fa {{ $page->icon }} sidebar-icon"></span>
+        @endif
+        {{ $indent . $page->name }}
+      </a>
+    	@if ($children = $community->getPages($page->id))
+        <ul>
+        	@include('frontend.communities.show.sidebar.pages', array('community' => $community, 'pages' => $children, 'slug' => $slug . '/' . $page->slug, 'indent' => $indent . '&nbsp;&nbsp;'))
+  		  </ul>
       @endif
-      {{ $indent . $page->name }}
-    </a>
-  	@if ($children = $community->getPages($page->id))
-      <ul>
-      	@include('frontend.communities.show.sidebar.pages', array('community' => $community, 'pages' => $children, 'slug' => $slug . '/' . $page->slug, 'indent' => $indent . '&nbsp;&nbsp;'))
-		  </ul>
-    @endif
-  </li>
+    </li>
+  @endif
 @endforeach
